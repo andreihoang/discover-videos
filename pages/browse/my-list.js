02157ca7b60e8aaffebc312cd06a4c8a5ee0909b
Dang Huy Hoang <dang_huy.hoang@student.unsw.edu.au>
2022-10-13 19:53:46 +0700

    change redirect 2

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -3,10 +3,10 @@ import Head from "next/head";
 import SectionCards from "../../components/Card/sectionCards";
 import styles from "../../styles/MyList.module.css";
 import { getMyList } from "../../lib/videos";
-import useRedirectUser from "../../utils/redirectUser";
+import { redirectUser } from "../../utils/redirectUser";
 
 export async function getServerSideProps(context) {
-  const { userId, token } = await useRedirectUser(context);
+  const { userId, token } = await redirectUser(context);
   if (!userId) {
     return {
       props: {},
